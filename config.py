3175f89c04eb51cd814e5e6391117de2b33a37b5
Dmytro <git20121983@gmail.com>
2026-01-04 12:29:01 +0000

    conf

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 # config.py
 
-BOT_TOKEN = "Т8133529792:AAGfz8tC8JhGQe7kMBVi1j_DeBZpeo4wlGk"
+BOT_TOKEN = "8133529792:AAGfz8tC8JhGQe7kMBVi1j_DeBZpeo4wlGk"
+
 MAX_RESULTS = 5
+
 LARDI_COOKIES = {
     "lardi_login": "Reno3",
     "lardi_mlogin": "eyJkYXRhIjpbeyJsb2dpbiI6IlJlbm8zIiwiYWlkIjoiZjM3Y2NlNmQtNWQ0Mi00NzEyLThmN2UtNTcxMWIzM2QwMzA1IiwidGltZSI6MTc2NzUyMjU1MjExOSwicmVmSWQiOjE4OTI3Njc4NTA4fV19",
